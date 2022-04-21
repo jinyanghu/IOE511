e736c8b2eb2c1726d2505ec0845546946c14dd92
clchiang <ginochiang2010@gmail.com>
2022-04-21 12:31:57 -0400

    Accelerate Hessian calculation

--- problems/P11_Exponential_1000/exponential_1000_Hess.m
+++ problems/P11_Exponential_1000/exponential_1000_Hess.m
@@ -1,11 +1,9 @@
 function [H] = exponential_1000_Hess(x)
 %EXPONENTIAL_1000_HESS Summary of this function goes here
 %   Detailed explanation goes here
-H = zeros(1000,1000);
-H(1,1) = 2*(exp(x(1)) * (exp(x(1)) + 1)^2 - (exp(x(1)) + 1) * exp(x(1)))  / (exp(x(1)) + 1)^4 + 0.1*exp(-x(1));
-       %H(1,1) = 2*(-exp(2*x(1))+exp(x(1)))/(exp(x(1))+1)^3 + 0.1*exp(-x(1));
-for i = 2:1000
-    H(i,i) = 12*(x(i) - 1)^2;
-end
+
+H = diag(12*(x-1).^2);
+H(1,1) = 2*(-exp(2*x(1))+exp(x(1)))/(exp(x(1))+1)^3 + 0.1*exp(-x(1));
+
 end
 
