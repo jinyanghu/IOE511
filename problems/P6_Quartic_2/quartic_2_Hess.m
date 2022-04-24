0bf189dc4f08be8b2df1b29e6d4ecfa2580f38d2
clchiang <ginochiang2010@gmail.com>
2022-04-24 10:33:24 -0400

    Fix p6 typo: eyes -> eye

--- problems/P6_Quartic_2/quartic_2_Hess.m
+++ problems/P6_Quartic_2/quartic_2_Hess.m
@@ -10,6 +10,6 @@ Q = [5 1 0 0.5;
 sigma = 1e4;
 
 % compute function value
-H = eyes(size(x,1)) + 2*sigma*(Q*x)*(x'*Q) + sigma*(x'*Q*x)*Q;
+H = eye(size(x,1)) + 2*sigma*(Q*x)*(x'*Q) + sigma*(x'*Q*x)*Q;
 end
 
